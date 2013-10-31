4d9e44852bd24e67eeb7739ec5d8b9f0b1898b5a
Tyler Nappy <tylernappy@gmail.com>
2013-10-31 15:16:34 -0400

    newest commit

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '751c300dab7ff500feca61cb38032c7114c7dbba7884ef84a70a2a0ce6c9f8e2b3b1ab7512dadccd215f8a4c7ef6cef7b890707a5be5df55343c47c5e988b6d4'
---
+++ test_heroku/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestHeroku::Application.config.secret_key_base = 'df71632653e85437b0da6a47cf0d151b8b57117660391fbdf14a80a7bbfbda215d67811c9b3cd40e78538e9f4c140031c6524449a290502ff2976a173c95f64c'
